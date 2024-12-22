ebf90746a9c9994ab1448fe86ff0a61ed5d95621
vishwaravi <vishwaman45@gmail.com>
2024-12-22 17:36:12 +0530

    New Blog Post on 2024-12-22 17:36:12

--- push-site.sh
+++ push-site.sh
@@ -67,29 +67,29 @@ if ! hugo; then
     exit 1
 fi
 
-# # Step 5: Add changes to Git
-# echo "Staging changes for Git..."
-# if git diff --quiet && git diff --cached --quiet; then
-#     echo "No changes to stage."
-# else
-#     git add .
-# fi
+# Step 5: Add changes to Git
+echo "Staging changes for Git..."
+if git diff --quiet && git diff --cached --quiet; then
+    echo "No changes to stage."
+else
+    git add .
+fi
 
-# # Step 6: Commit changes with a dynamic message
-# commit_message="New Blog Post on $(date +'%Y-%m-%d %H:%M:%S')"
-# if git diff --cached --quiet; then
-#     echo "No changes to commit."
-# else
-#     echo "Committing changes..."
-#     git commit -m "$commit_message"
-# fi
+# Step 6: Commit changes with a dynamic message
+commit_message="New Blog Post on $(date +'%Y-%m-%d %H:%M:%S')"
+if git diff --cached --quiet; then
+    echo "No changes to commit."
+else
+    echo "Committing changes..."
+    git commit -m "$commit_message"
+fi
 
-# # Step 7: Push all changes to the main branch
-# echo "Deploying to GitHub Main..."
-# if ! git push origin main; then
-#     echo "Failed to push to main branch."
-#     exit 1
-# fi
+# Step 7: Push all changes to the main branch
+echo "Deploying to GitHub Main..."
+if ! git push origin main; then
+    echo "Failed to push to main branch."
+    exit 1
+fi
 
 # # Step 8: Push the public folder to the hostinger branch using subtree split and force push
 # echo "Deploying to GitHub Hostinger..."
@@ -110,4 +110,4 @@ fi
 
 # git branch -D hostinger-deploy
 
-# echo "All done! Site synced, processed, committed, built, and deployed."
+echo "All done! Site synced, processed, committed, built, and deployed."
